cc893e23e02cc79fd8715a9bbcdc0c9e1b734c91
planttheidea <tony.quetano@planttheidea.com>
2014-11-20 10:11:33 -0500

    Update sup.js

--- sup.js
+++ sup.js
@@ -1,3 +1,17 @@
+/*
+ *
+ * Copyright 2014 Tony Quetano under the terms of the MIT
+ * license found at https://github.com/planttheidea/sup/MIT_License.txt
+ *
+ * sup.js - A simple publish / subscribe library without dependencies
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
 (function(window,document){
 	var topics = {},
 		IDs = {},
