16b19e1f3d9b5c8b3def5215a0032e83f5c67648
Plant The Idea <tony.quetano@planttheidea.com>
2015-06-07 11:16:59 -0400

    Update sup.js

--- sup.js
+++ sup.js
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2014 Tony Quetano under the terms of the MIT
+ * Copyright 2015 Tony Quetano under the terms of the MIT
  * license found at https://github.com/planttheidea/sup/MIT_License.txt
  *
  * sup.js - A simple publish / subscribe library without dependencies
@@ -12,21 +12,14 @@
  * limitations under the License.
  *
 */
-(function(window,document){
+;(function(window,document,undefined){
 	var topics = {},
 		IDs = {},
+		persistentIDs = {},
 		subUid = -1,
-		throwError = function(e){
-			throw new Error(e);
-		},
-		type = function(obj){
+		getType = function(obj){
 			return Object.prototype.toString.call(obj).replace(/^\[object (.+)\]$/, "$1").toLowerCase();
 		};
-	
-	// function to retrieve internal ID assigned to subscription
-	function prv_getID(idObj){
-		return IDs[idObj.name];
-	}
 	
 	// publishes event, with data and topic as arguments
 	function prv_publish(publishObj){
@@ -54,36 +47,29 @@
 	 */
 	function prv_unsubscribeName(name){
 		if(IDs[name] > 0){
-			for(var m in topics){
+                        for(var m in topics){
 				if(topics[m]){
-					for (var i = topics[m].length; i--;) {			
-						if(topics[m][i].token === IDs[name]){
+					for (var i = topics[m].length; i--;) {
+						if((topics[m][i].token === IDs[name]) && !topics[m][i].persistent){
 							delete IDs[name];
-							
 							topics[m].splice(i,1);
 						}
 					}
 				}
-			}
-		}
+                        }
+                }
 	}
 	
 	// API access, calls prv_unsubscribeName differently depending on type
 	function prv_unsubscribe(unsubscribeObj){
-		switch(type(unsubscribeObj.name)){
-			case 'string':
-				prv_unsubscribeName(unsubscribeObj.name);
-				
-				break;
-			case 'array':
-				for(var i = unsubscribeObj.name.length; i--;){
-					prv_unsubscribeName(unsubscribeObj.name[i]);
-				}
-				
-				break;
-			default:
-				throwError('Name passed is not of valid type.');
-				break;
+		var type = getType(unsubscribeObj.name);
+		
+		if(type === 'string'){
+			prv_unsubscribeName(unsubscribeObj.name);
+		} else if(type === 'array'){
+			for(var i = unsubscribeObj.name.length; i--;){
+				prv_unsubscribeName(unsubscribeObj.name[i]);
+			}
 		}
 		
 		return this;
@@ -91,7 +77,7 @@
 	
 	// performs subscription (abstrated for the same reason as above unsubscription)
 	function prv_subscribeTopic(topic,newToken,fn,once,name){
-		if(type(topics[topic]) !== 'array'){
+		if(getType(topics[topic]) !== 'array'){
 			topics[topic] = [];
 		}
 		
@@ -113,52 +99,32 @@
 	 * to the topics provided
 	 */
 	function prv_subscribe(subscribeObj){
-		// throws an error if the name passed is not a string
-		if(type(subscribeObj.name) !== 'string'){
-			throwError('Name passed is not a string.');
-			return false;
-		}
+                // only subscribes if name is provided
+		if(getType(subscribeObj.name) == 'string'){
+			var type = getType(unsubscribeObj.topic);
+			// unsubscribes from topic if subscription already exists
+			if(IDs[subscribeObj.name]){
+				prv_unsubscribeName(subscribeObj.name);
+			}
 		
-		// unsubscribes from topic if subscription already exists
-		if(IDs[subscribeObj.name]){
-			prv_unsubscribeName(subscribeObj.name);
-		}
-		
-		// subscriptions called differently depending on typ
-		switch(type(subscribeObj.topic)){
-			case 'string':
+			if(type === 'string'){
 				prv_subscribeTopic(subscribeObj.topic,subscribeObj.token,subscribeObj.fn,subscribeObj.once,subscribeObj.name);
-
-				break;
-			case 'array':					
+			} else if(type === 'array'){
 				for(var i = subscribeObj.topic.length; i--;){
 					prv_subscribeTopic(subscribeObj.topic[i],subscribeObj.token,subscribeObj.fn,subscribeObj.once,subscribeObj.name);
 				};
-				
-				break;
-			case 'undefined':
-				throwError('Must provide a topic to subscribe to.');
-				
-				break;
-			default:
-				throwError('Invalid topic type, must be either string or array.');		
-								
-				break;
+			}
+			
+			// assigns new ID
+			IDs[subscribeObj.name] = (++subUid);
 		}
-		
-		// assigns new ID
-		IDs[subscribeObj.name] = (++subUid);
 		
 		return this;
 	}
 	
-	if(!window.Sup){
-		window.Sup = {
-			publish:prv_publish,
-			subscribe:prv_subscribe,
-			unsubscribe:prv_unsubscribe
-		};
-	} else {
-		throwError('Window object of Sup already exists.');
-	}
+	window.Sup = {
+		publish:prv_publish,
+		subscribe:prv_subscribe,
+		unsubscribe:prv_unsubscribe
+	};
 })(window,document);
